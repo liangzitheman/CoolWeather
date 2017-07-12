da0201557612052472eb5a20f971fa2fb38d96c1
liangzi <liangzi@liangzideMacBook-Pro.local>
2017-07-12 14:01:02 +0800

    加入查询显示天气的功能

--- app/src/main/java/com/example/liangzi/coolweather/Activity/ChooseAreaActivity.java
+++ app/src/main/java/com/example/liangzi/coolweather/Activity/ChooseAreaActivity.java
@@ -2,11 +2,15 @@ package com.example.liangzi.coolweather.Activity;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -68,24 +72,38 @@ public class ChooseAreaActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (prefs.getBoolean("city_selected", false)) {
+            Intent intent = new Intent(this, WeatherActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.choose_area);
         listView = (ListView) findViewById(R.id.list_view);
         titleText = (TextView) findViewById(R.id.title_text);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_
-                list_item_1, dataList);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataList);
         listView.setAdapter(adapter);
         coolWeatherDB = CoolWeatherDB.getInstance(this);
         listView.setOnItemClickListener(new OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> arg0, View view, int index,
-                                    long arg3) {
+            public void onItemClick(AdapterView<?> arg0, View view, int index, long arg3) {
                 if (currentLevel == LEVEL_PROVINCE) {
                     selectedProvince = provinceList.get(index);
                     queryCities();
                 } else if (currentLevel == LEVEL_CITY) {
                     selectedCity = cityList.get(index);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY) {
+                    String countyCode = countyList.get(index).getCountyCode();
+                    Intent intent = new Intent(ChooseAreaActivity.this,
+                            WeatherActivity.class);
+                    intent.putExtra("county_code", countyCode);
+                    startActivity(intent);
+                    finish();
                 }
             }
         });
--- app/src/main/java/com/example/liangzi/coolweather/Util/Utility.java
+++ app/src/main/java/com/example/liangzi/coolweather/Util/Utility.java
@@ -1,11 +1,21 @@
 package com.example.liangzi.coolweather.Util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.example.liangzi.coolweather.DB.CoolWeatherDB;
 import com.example.liangzi.coolweather.Model.City;
 import com.example.liangzi.coolweather.Model.County;
 import com.example.liangzi.coolweather.Model.Province;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Locale;
+import java.util.Date;
+import java.text.*;
 
 /**
  * Created by liangzi on 2017/7/12.
@@ -79,4 +89,46 @@ public class Utility {
         }
         return false;
     }
+
+    /**
+     * 解析服务器返回的天气JSON数据，并将解析出的天气数据存储到本地。
+     */
+    public static void handleWeatherResponse(Context context, String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            String cityName = weatherInfo.getString("city");
+            String weatherCode = weatherInfo.getString("cityid");
+            String temp1 = weatherInfo.getString("temp1");
+            String temp2 = weatherInfo.getString("temp2");
+            String weatherDesp = weatherInfo.getString("weather");
+            String publishTime = weatherInfo.getString("ptime");
+            saveWeatherInfo(context, cityName, weatherCode, temp1, temp2,
+                    weatherDesp, publishTime);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 将服务器返回的所有天气信息存储到SharedPreferences文件中。
+     */
+    public static void saveWeatherInfo(Context context, String cityName,
+                                       String weatherCode, String temp1, String temp2, String weatherDesp, String
+                                               publishTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日",
+                Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager
+                .getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected", true);
+        editor.putString("city_name", cityName);
+        editor.putString("weather_code", weatherCode);
+        editor.putString("temp1", temp1);
+        editor.putString("temp2", temp2);
+        editor.putString("weather_desp", weatherDesp);
+        editor.putString("publish_time", publishTime);
+        editor.putString("current_date", sdf.format(new Date()));
+        editor.commit();
+    }
+
 }
