1eff84badb6901622484e32b80894205d15574a4
liangzi <liangzi@liangzideMacBook-Pro.local>
2017-07-12 10:40:05 +0800

    创建了数据库帮助类以及一些实体类

---
+++ app/src/main/java/com/example/liangzi/coolweather/Model/Province.java
@@ -0,0 +1,31 @@
+package com.example.liangzi.coolweather.Model;
+
+/**
+ * Created by liangzi on 2017/7/12.
+ */
+
+public class Province {
+
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getProvinceName() {
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
